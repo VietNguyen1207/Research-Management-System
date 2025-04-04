dd8f5bec53362457f040bb73b048580a3d138edd
unknown <vietnhse140570@fpt.edu.vn>
2025-04-04 16:50:23 +0700

    set up Vercel env

--- src/features/auth/authApiSlice.js
+++ src/features/auth/authApiSlice.js
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const apiUrl = import.meta.env.VITE_API_URL || "https://localhost:7045/api";
+
 // Create a dedicated API slice for auth
 export const authApiSlice = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://localhost:7045/api",
+    baseUrl: apiUrl,
   }),
   endpoints: (builder) => ({
     login: builder.mutation({
